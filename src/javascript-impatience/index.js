0b27a8fa97aa4dfe140a642ce396aa3b3526aef2
Sean <ussa.say@gmail.com>
2023-04-15 19:41:35 -0700

    Unknown error code 3 ???

---
+++ src/javascript-impatience/index.js
@@ -0,0 +1,13 @@
+// node.exe index.js COM7
+
+import { SerialPort } from 'serialport'
+
+const [, portPath] = process.argv
+
+const port = new SerialPort({
+  path: portPath,
+  baudRate: 115200
+})
+port.on('readable', function () {
+  console.log(port.read())
+})
