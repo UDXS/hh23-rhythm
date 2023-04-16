24255264df3d964f6fd70b1dea9435e0a2054858
Sean <ussa.say@gmail.com>
2023-04-15 20:15:47 -0700

    okay it was because i npm installed in wsl

--- src/javascript-impatience/index.js
+++ src/javascript-impatience/index.js
@@ -1,16 +1,15 @@
-// node.exe index.js COM7
+// node index.js COM7
 
 import { SerialPort } from 'serialport'
 
+const decoder = new TextDecoder()
+
 const [, , portPath] = process.argv
-console.log(portPath)
+
 const port = new SerialPort({
   path: portPath,
   baudRate: 115200
 })
 port.on('readable', () => {
-  console.log(port.read())
-})
-port.on('close', () => {
-  console.log('close')
+  console.log(decoder.decode(port.read(80)))
 })
