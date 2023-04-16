90c688ba9f61534702ce554b0a20605c19253d83
Sean <ussa.say@gmail.com>
2023-04-15 19:50:23 -0700

    why does it keep auto closing

--- src/javascript-impatience/index.js
+++ src/javascript-impatience/index.js
@@ -7,6 +7,15 @@ const port = new SerialPort({
   path: portPath,
   baudRate: 115200
 })
+port.on('open', () => {
+  console.log('open')
+})
 port.on('readable', () => {
   console.log(port.read())
+})
+port.on('data', data => {
+  console.log(data)
+})
+port.on('close', () => {
+  console.log('close')
 })
