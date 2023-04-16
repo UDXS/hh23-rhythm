09e0bd3843d17d0279e6dca849401a31b322ff1e
Sean <ussa.say@gmail.com>
2023-04-15 19:28:51 -0700

    powershell leaves ports open when its process dies I think
    oh maybe if I just pipe it into stdin

--- src/javascript-impatience/main.ts
+++ src/javascript-impatience/main.ts
@@ -1,16 +1,28 @@
-// deno run --allow-run=cmd.exe main.ts
+// deno run --allow-run=powershell.exe main.ts
 
 import { writeAll } from 'https://deno.land/std@0.183.0/streams/write_all.ts'
 
 const serialPort = 'COM7'
 
 const process = Deno.run({
-  cmd: ['cmd.exe', '/c', 'type', serialPort],
+  cmd: ['powershell.exe'],
+  stdin: 'piped',
   stdout: 'piped'
-  // stderr: 'null'
 })
 
+const encoder = new TextEncoder()
 const decoder = new TextDecoder()
-for await (const chunk of process.stdout.readable) {
-  console.log('!>', decoder.decode(chunk), '<!')
+
+await writeAll(
+  process.stdin,
+  encoder.encode(
+    `$port = new-Object System.IO.Ports.SerialPort ${serialPort},115200,None,8,one\n$port.Open();\n`
+  )
+)
+
+while (true) {
+  await writeAll(process.stdin, encoder.encode(`$port.ReadLine();\n`))
+  const arr = new Uint8Array(80)
+  await process.stdout.read(arr)
+  console.log(decoder.decode(arr))
 }
