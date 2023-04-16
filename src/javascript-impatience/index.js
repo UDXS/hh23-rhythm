1d589767c2a0cfb895d2957c47c02f862bf714e4
Sean <ussa.say@gmail.com>
2023-04-15 20:00:22 -0700

    hmmst

--- src/javascript-impatience/index.js
+++ src/javascript-impatience/index.js
@@ -3,18 +3,13 @@
 import { SerialPort } from 'serialport'
 
 const [, , portPath] = process.argv
+console.log(portPath)
 const port = new SerialPort({
   path: portPath,
   baudRate: 115200
 })
-port.on('open', () => {
-  console.log('open')
-})
 port.on('readable', () => {
   console.log(port.read())
-})
-port.on('data', data => {
-  console.log(data)
 })
 port.on('close', () => {
   console.log('close')
