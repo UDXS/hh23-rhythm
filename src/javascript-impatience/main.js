67c629884073bdfabf76d227adf602cf82aa8a2f
Sean <ussa.say@gmail.com>
2023-04-15 21:49:10 -0700

    3d??

--- src/javascript-impatience/main.js
+++ src/javascript-impatience/main.js
@@ -1,9 +1,3 @@
-await new Promise(resolve =>
-  document.addEventListener('click', resolve, { once: true })
-)
-const port = await navigator.serial.requestPort()
-await port.open({ baudRate: 115200 })
-
 const PACKET_LENGTH = 12
 const MAX_SLIDER = 300
 const ZERO = '0'.codePointAt(0)
@@ -49,13 +43,54 @@ async function * packets (port) {
 }
 
 const wow = document.getElementById('wow')
-const lmao = document.getElementById('lmao')
+const left = document.getElementById('left')
+const right = document.getElementById('right')
 
-for await (const entry of packets(port)) {
-  lmao.textContent = [
-    entry.leftBtn ? '✅' : '❌',
-    entry.rightBtn ? '✅' : '❌'
-  ].join(' ')
-  wow.style.width = entry.sliderPos * 100 + '%'
-  wow.style.opacity = entry.sliderPressed ? 1 : 0.5
+let tilt = 0
+let targetTilt = 0
+
+// Based on https://github.com/SheepTester/words-go-here/blob/master/misc/artificial-thonkulos/render-simulation.mjs
+const SIM_STEP = 5 // ms per simulation
+const MAX_DELAY = 500 // ms
+let idealSimTime = 0
+let simulatedTime = 0
+function simulate () {
+  tilt += (targetTilt - tilt) * 0.1
 }
+
+let lastTime = Date.now()
+function paint () {
+  const now = Date.now()
+  const elapsed = now - lastTime
+  if (elapsed < MAX_DELAY) {
+    idealSimTime += elapsed
+    while (simulatedTime < idealSimTime) {
+      simulate()
+      simulatedTime += SIM_STEP
+    }
+  }
+  lastTime = now
+  wow.style.setProperty('--tilt', `${tilt}deg`)
+  window.requestAnimationFrame(paint)
+}
+paint()
+
+async function connect () {
+  document.getElementById('note').remove()
+  const port = await navigator.serial.requestPort()
+  await port.open({ baudRate: 115200 })
+  for await (const entry of packets(port)) {
+    if (entry.leftBtn) {
+      left.classList.add('active')
+    } else {
+      left.classList.remove('active')
+    }
+    if (entry.rightBtn) {
+      right.classList.add('active')
+    } else {
+      right.classList.remove('active')
+    }
+    targetTilt = entry.sliderPressed ? entry.sliderPos * 20 - 10 : 0
+  }
+}
+document.addEventListener('click', connect, { once: true })
